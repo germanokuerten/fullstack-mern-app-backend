e77c732b1f993d94c81382f26e0047f29945dace
Germano Kuerten <germanokuerten@gmail.com>
2022-05-24 20:32:26 -0700

    update

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 // Dependencies
-
 require("dotenv").config()
 
 const PORT = process.env.PORT || 3001
@@ -7,13 +6,11 @@ const express = require("express")
 const app = express()
 
 // Routes
-
 app.get("/", (req, res) => {
     res.send("App is active")
 })
 
 // Listener
-
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 })
